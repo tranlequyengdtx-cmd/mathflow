d7e8531294723e9a0e925551fa62b6f4767220f9
tranlequyengdtx-cmd <tranlequyen_gdtx@bentre.edu.vn>
2026-09-15 07:49:38 +0700

    update

--- questions.js
+++ questions.js
@@ -2,13 +2,13 @@ window.mathflowData = {
   "questions": [
     {
       "id": "q1",
-      "content": "Cho tập hợp $A={n∈N∣1<n<5}$. Viết tập hợp $A$ dưới dạng liệt kê các phần tử.\n\n[!info]\nCác số tự nhiên n thỏa mãn điều kiện $1<n<5$ là $2,3,4$.\nDo đó, tập hợp $A$ được viết dưới dạng liệt kê các phần tử là $A=\\{2;3;4\\}.",
+      "content": "Cho tập hợp $A={n\\in\\mathbb N\\mid 1<n<5}$, viết tập hợp $A$ dưới dạng liệt kê các phần tử.\n\n[!info]\nCác số tự nhiên n thỏa mãn điều kiện $1<n<5$ là $2,3,4$.\nDo đó, tập hợp $A$ được viết dưới dạng liệt kê các phần tử là $A=\\{2;3;4\\}.",
       "type": "mcq",
       "options": [
-        "A=\\{2;3;4\\}.",
-        "A=\\{1;2;3;4\\}.",
-        "A=\\{2;3;4;5\\}.",
-        "A=\\{1;2;3;4;5\\}."
+        "A=\\{2;3;4\\}",
+        "A=\\{1;2;3;4\\}",
+        "A=\\{2;3;4;5\\}",
+        "A=\\{1;2;3;4;5\\}"
       ],
       "answer": "0ed2c60216f38a9ad688d9136b5297ec9790b1c39c37e3c33ac5e1fd5dce4109",
       "explanation": "",
@@ -25,10 +25,10 @@ window.mathflowData = {
       "content": "Cho tập hợp $A = \\{x \\in \\mathbb{R} \\mid x^2 - 4 = 0\\}$. Viết tập hợp $A$ dưới dạng liệt kê các phần tử.",
       "type": "mcq",
       "options": [
-        "$A = \\{-2\\}$.",
-        "$A = \\{2\\}$.",
-        "$A = \\{-2; 2\\}$.",
-        "$A = \\{4\\}$."
+        "$A = \\{-2\\}$",
+        "$A = \\{2\\}$",
+        "$A = \\{-2; 2\\}$",
+        "$A = \\{4\\}$"
       ],
       "answer": "a76cf3be595610ad159843632571857d167fc953faded4329d673c512488afea",
       "explanation": "OQBUC8KVTEsPAUFFTlJRVGISABJqIQQSHBQFCB8rEhcRHRJUJxINEmodDFRaQkJPIeG7sgpFRzNFSX9uSx8EVkFGNBtIQQ==",
@@ -45,10 +45,10 @@ window.mathflowData = {
       "content": "Cho tập hợp $A = \\{1; 3; 5; 7\\}$. Khẳng định nào sau đây là **đúng**?",
       "type": "mcq",
       "options": [
-        "$3 \\in A$.",
-        "$6 \\in A$.",
-        "$\\{1; 3\\} \\in A$.",
-        "$5 \\notin A$."
+        "$3 \\in A$",
+        "$6 \\in A$",
+        "$\\{1; 3\\} \\in A$",
+        "$5 \\notin A$"
       ],
       "answer": "0ed2c60216f38a9ad688d9136b5297ec9790b1c39c37e3c33ac5e1fd5dce4109",
       "explanation": "PuG6sFRbRhgHAuG6hhBFF+G7nxVUN+G7kUASEixFVAbCjAJPU2xTOQocRTV7ElzDkhYGCeG7hwYBTMW+4bq8MRtFxbLCiAsTcQ==",
@@ -65,10 +65,10 @@ window.mathflowData = {
       "content": "Cho hai tập hợp $A = \\{1; 2; 3; 4\\}$ và $B = \\{2; 4; 6\\}$. Tìm tập hợp $A \\cap B$.",
       "type": "mcq",
       "options": [
-        "$\\{1; 2; 3; 4; 6\\}$.",
-        "$\\{2; 4\\}$.",
-        "$\\{1; 3\\}$.",
-        "$\\{6\\}$."
+        "$\\{1; 2; 3; 4; 6\\}$",
+        "$\\{2; 4\\}$",
+        "$\\{1; 3\\}$",
+        "$\\{6\\}$"
       ],
       "answer": "90d7906992416673b28c73ac445c47c07a28165acdfdc93743be309d1f609f86",
       "explanation": "KggVB0YP4bqIFn8bBApSEeG7mS8SWOG7kUZNRTVIOg8OB38xQUMRDeG6nT4SU8OTVU0RHOG7jwhMG+G6mn8QDRYcAlQ84buVURISLEVUHsKGTEs1e1MJwoNSQUZ7EkbDkhZJVVBG",
@@ -85,10 +85,10 @@ window.mathflowData = {
       "content": "Kí hiệu nào sau đây dùng để chỉ tập hợp các số tự nhiên?",
       "type": "mcq",
       "options": [
-        "$\\mathbb{N}$.",
-        "$\\mathbb{Z}$.",
-        "$\\mathbb{Q}$.",
-        "$\\mathbb{R}$."
+        "$\\mathbb{N}$",
+        "$\\mathbb{Z}$",
+        "$\\mathbb{Q}$",
+        "$\\mathbb{R}$"
       ],
       "answer": "0ed2c60216f38a9ad688d9136b5297ec9790b1c39c37e3c33ac5e1fd5dce4109",
       "explanation": "OeG7jARIDuG6jx9XPMKSBkMB4bq0VCvhu4MQXF4EwosaSMW3x5zhuowUfxjCiEMaDOG6syoSXMOSFkk9GQkSBA0VJD0YR1w=",
@@ -102,7 +102,7 @@ window.mathflowData = {
     },
     {
       "id": "q6",
-      "content": "Ký hiệu nào sau đây biểu diễn nửa khoảng $[2; 5)$?\n\\choicess{4}{*$\\{x \\in \\mathbb{R} \\mid 2 \\le x < 5\\}$.}{$\\{x \\in \\mathbb{R} \\mid 2 < x < 5\\}$.}{$\\{x \\in \\mathbb{R} \\mid 2 \\le x \\le 5\\}$.}{$\\{x \\in \\mathbb{R} \\mid 2 < x \\le 5\\}$.}",
+      "content": "Ký hiệu nào sau đây biểu diễn nửa khoảng $[2; 5)$?\n\\choicess{4}\n{*$\\{x \\in \\mathbb{R} \\mid 2 \\le x < 5\\}$}\n{$\\{x \\in \\mathbb{R} \\mid 2 < x < 5\\}$}\n{$\\{x \\in \\mathbb{R} \\mid 2 \\le x \\le 5\\}$}\n{$\\{x \\in \\mathbb{R} \\mid 2 < x \\le 5\\}$}",
       "type": "short_answer",
       "options": [],
       "answer": "0ed2c60216f38a9ad688d9136b5297ec9790b1c39c37e3c33ac5e1fd5dce4109",
@@ -117,13 +117,13 @@ window.mathflowData = {
     },
     {
       "id": "q7",
-      "content": "Cho tập hợp $A = [-2; 3)$ và $B = (1; 5]$. Tìm tập hợp $A \\cup B$.",
+      "content": "Cho tập hợp $A = [-2; 3)$ và $B = (1; 5]$. Xác định tập hợp $A \\cup B$.",
       "type": "mcq",
       "options": [
-        "$(1; 3)$.",
-        "$[-2; 5)$.",
-        "$[-2; 5]$.",
-        "$(1; 5]$."
+        "$(1; 3)$",
+        "$[-2; 5)$",
+        "$[-2; 5]$",
+        "$(1; 5]$"
       ],
       "answer": "a76cf3be595610ad159843632571857d167fc953faded4329d673c512488afea",
       "explanation": "JeG6ggRIBeG6iw5XNxIMQwbhu4gEf1rhu5NCFkkgVDQFGR9XHVNYQylIRmQSAxsWMQIBGEZEXkx/RjhDT0UvcgALEgMwRVo=",
@@ -140,10 +140,10 @@ window.mathflowData = {
       "content": "Cho tập hợp $A = (-\\infty; 4]$ và $B = (0; +\\infty)$. Tìm hiệu $A \\setminus B$.",
       "type": "mcq",
       "options": [
-        "$(-\\infty; 0]$.",
-        "$(0; 4]$.",
-        "$(-\\infty; 0)$.",
-        "$(4; +\\infty)$."
+        "$(-\\infty; 0]$",
+        "$(0; 4]$",
+        "$(-\\infty; 0)$",
+        "$(4; +\\infty)$"
       ],
       "answer": "0ed2c60216f38a9ad688d9136b5297ec9790b1c39c37e3c33ac5e1fd5dce4109",
       "explanation": "JQjhurMdRkguVwMAABcfDBoqQRBwFlBBXEU6BQERKwpeQ0Y4VANBVUZbBA8BG0ZEX0x/WDkKHAMAJhsQDxZFTCgBCAobDmRTVT5WSw==",
@@ -160,10 +160,10 @@ window.mathflowData = {
       "content": "Cho tập hợp $A = \\{1; 2; 3\\}$. Tập hợp $A$ có tất cả bao nhiêu tập hợp con?",
       "type": "mcq",
       "options": [
-        "$3$.",
-        "$6$.",
-        "$8$.",
-        "$9$."
+        "$3$",
+        "$6$",
+        "$8$",
+        "$9$"
       ],
       "answer": "a76cf3be595610ad159843632571857d167fc953faded4329d673c512488afea",
       "explanation": "PuG6sFQc4buLHE8f4bq8A0UAHQtUPOG7lVESQuG7gBFUAOG6hRxPEOG6jB5FRxxBVC9a4bqXXBYZ4bqMVATChkxLRQEdQU1S4bq7VMWOw5BJEhIDQUlIVUwzJTYUDRcTFwYwRRAAaF5BSUheSEE=",
@@ -180,10 +180,10 @@ window.mathflowData = {
       "content": "Cho $A = \\{x \\in \\mathbb{R} \\mid x - 1 > 0\\}$ và $B = \\{x \\in \\mathbb{R} \\mid x + 2 \\le 5\\}$. Tìm $A \\cap B$.",
       "type": "mcq",
       "options": [
-        "$(1; 3)$.",
-        "$(1; 3]$.",
-        "$[1; 3]$.",
-        "$(-\\infty; 3]$."
+        "$(1; 3)$",
+        "$(1; 3]$",
+        "$[1; 3]$",
+        "$(-\\infty; 3]$"
       ],
       "answer": "90d7906992416673b28c73ac445c47c07a28165acdfdc93743be309d1f609f86",
       "explanation": "OQBUC8KVTEs2f05FS0NeVHRuWVxQGRhdTEYawo9XezFFXlJNWQNbXlRCFFpUWztIQVcbHEXFssKBRVAeEmxRVx1BNkhbTEdGZFNWPlZL",
@@ -200,10 +200,10 @@ window.mathflowData = {
       "content": "Lớp 10A có 40 học sinh, trong đó có 25 học sinh thích môn Toán, 20 học sinh thích môn Văn và 8 học sinh không thích môn nào trong hai môn này. Hỏi có bao nhiêu học sinh thích cả hai môn Toán và Văn?",
       "type": "mcq",
       "options": [
-        "$5$.",
-        "$10$.",
-        "$13$.",
-        "$15$."
+        "$5$",
+        "$10$",
+        "$13$",
+        "$15$"
       ],
       "answer": "a76cf3be595610ad159843632571857d167fc953faded4329d673c512488afea",
       "explanation": "PuG6sFQA4bqrD08ENh0NQwYNwpk8WhDDn0JNDxzhu40STALhuq4rUwjClxxFIDDDk14SXgLhu5YXSDDFrwFXM8KTX0NWUUR/HxAKFlBBR1pCTAfhuro8UxYKHA1aVWHhu6ESXuG6oAJUGw8CB1crG8KIABpFF+G7vBJYU19NDMKABkYAwo9Nf1dXVlJOVG0CEB8WXlNUVUZdXFN/G+G6qABSFh0xWh4=",
